45cc049682d56e74944cbf042540d0b3f2d3b4a8
Brett Chaldecott <brett.chaldecott@gmail.com>
2019-03-11 14:59:42 +0200

    1) Finished base contract development

---
+++ assembly/account/account_query.ts
@@ -0,0 +1,62 @@
+import {Keto, ResultRow, Transaction} from "../../lib/typescript_contract_sdk/assembly/keto"
+import {Constants} from "../constants"
+import {TsJSONBuilder} from "../../lib/typescript_contract_sdk/assembly/json/TsJSONBuilder"
+
+
+export class AccountQuery {
+    
+    transaction: Transaction;
+    accountHash: string;
+    debits: i64 = 0;
+    credits: i64 = 0;
+    
+    constructor() {
+        this.transaction = Keto.transaction();
+        this.accountHash = this.transaction.getAccount();
+
+        // calculate the total credits to this account
+        let creditTotal = Keto.executeQuery("SELECT (SUM(?value) as ?totalValue) WHERE { " +
+            "?transaction <http://keto-coin.io/schema/rdf/1.0/keto/AccountTransaction#accountHash> \"" + this.accountHash + "\"^^<http://www.w3.org/2001/XMLSchema#string> . " +
+            "?transaction <http://keto-coin.io/schema/rdf/1.0/keto/AccountTransaction#type> \"credit\"^^<http://www.w3.org/2001/XMLSchema#string> . " +
+            "?transaction <http://keto-coin.io/schema/rdf/1.0/keto/AccountTransaction#value> ?value . " +
+            "}",Keto.QUERY_TYPES.REMOTE)
+        
+        if (creditTotal.getRowCount()) {
+            this.credits = creditTotal.nextRow().getQueryLongByKey("totalValue")
+        }
+
+        // calculate the total debits to this account
+        let debitTotal = Keto.executeQuery("SELECT (SUM(?value) as ?totalValue) WHERE { " +
+            "?transaction <http://keto-coin.io/schema/rdf/1.0/keto/AccountTransaction#accountHash> \"" + this.accountHash + "\"^^<http://www.w3.org/2001/XMLSchema#string> . " +
+            "?transaction <http://keto-coin.io/schema/rdf/1.0/keto/AccountTransaction#type> \"debit\"^^<http://www.w3.org/2001/XMLSchema#string> . " +
+            "?transaction <http://keto-coin.io/schema/rdf/1.0/keto/AccountTransaction#value> ?value . " +
+            "}",Keto.QUERY_TYPES.REMOTE)
+        if (debitTotal.getRowCount()) {
+            this.debits =  debitTotal.nextRow().getQueryLongByKey("totalValue")
+        }
+    }
+
+    getTotal() : u64 {
+        return this.credits - this.debits;
+    }
+
+    accountTransactions(builder: TsJSONBuilder) : void {
+        let transactions = Keto.executeQuery("SELECT ?id ?blockId ?date ?account WHERE {" +
+            "?transaction <http://keto-coin.io/schema/rdf/1.0/keto/Transaction#id> ?id . " +
+            "?transaction <http://keto-coin.io/schema/rdf/1.0/keto/Transaction#block> ?block . " +
+            "?block <http://keto-coin.io/schema/rdf/1.0/keto/Block#id> ?blockId . " +
+            "?transaction <http://keto-coin.io/schema/rdf/1.0/keto/Transaction#date> ?date . " +
+            "?transaction <http://keto-coin.io/schema/rdf/1.0/keto/Transaction#account>  \"" + this.accountHash + "\"^^<http://www.w3.org/2001/XMLSchema#string> . " +
+            "?transaction <http://keto-coin.io/schema/rdf/1.0/keto/Transaction#account> ?account . " +
+            "} ORDER BY DESC(?date) LIMIT 10")
+
+        let row : ResultRow = null;
+        while ((row = transactions.nextRow()) != null) {
+            let jsonObj = builder.add();
+            jsonObj.add("id").set(row.getQueryStringByKey("id"))
+            jsonObj.add("blockId").set(row.getQueryStringByKey("blockId"))
+            jsonObj.add("account").set(this.accountHash)
+            jsonObj.add("date").set(row.getQueryStringByKey("date"))
+        }
+    }
+}
