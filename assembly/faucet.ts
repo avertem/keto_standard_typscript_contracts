6419b14072c68205f8af618641588e8ac28829d6
Brett Chaldecott <brett.chaldecott@absa.co.za>
2019-11-14 20:11:19 +0200

    1) Updated the faucet

--- assembly/faucet.ts
+++ assembly/faucet.ts
@@ -2,18 +2,33 @@
 //import "allocator/system";
 import {Keto, ResultRow, Transaction} from "../lib/typescript_contract_sdk/assembly/keto"
 import {TsJSON} from "../lib/typescript_contract_sdk/assembly/json/TsJSON"
+import {AccountQuery} from "./account/account_query"
 import {Constants} from "./constants"
 
 export {_malloc,_free} from "../lib/typescript_contract_sdk/assembly/keto"
 
+var KETO_NAME: string = "keto_account_faucet";
+var KETO_FAUCET_VALUE : u64 = 1000;
+
 export function debit(): bool {
-    Keto.log(Keto.LOG_LEVEL.ERROR,"[debit][faucet] ");
+    let transaction = Keto.transaction();
+    Keto.log(Keto.LOG_LEVEL.ERROR,"[debit][faucet] debit account [" + transaction.getAccount() + "]");
+
+    let accountQuery = new AccountQuery();
+    if (accountQuery.getTotal() < KETO_FAUCET_VALUE) {
+        return false;
+    }
+
+    transaction.createDebitEntry(transaction.getAccount(),KETO_NAME,"debit the faucet account",Constants.KETO_ACCOUNT_MODEL,Constants.KETO_ACCOUNT_TRANSACTION_MODEL,KETO_FAUCET_VALUE);
+
     return true;
 }
 
 export function credit(): bool {
     let transaction = Keto.transaction();
-    Keto.log(Keto.LOG_LEVEL.ERROR,"[credit][faucet] ");
+    Keto.log(Keto.LOG_LEVEL.DEBUG,"[credit][faucet][" + transaction.getAccount() + "]");
+    transaction.createCreditEntry(transaction.getAccount(),KETO_NAME,"credit the target account with faucet value",Constants.KETO_ACCOUNT_MODEL,Constants.KETO_ACCOUNT_TRANSACTION_MODEL,
+        KETO_FAUCET_VALUE - transaction.getTotalFeeValue(Constants.KETO_MIMIMIM_FEE));
     return true;
 }
 
